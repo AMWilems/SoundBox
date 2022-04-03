78f7d02fe1bab380eaabb45d75fe37cef63f87b2
Alexander Wilems <alexander.wilems@gmail.com>
2022-04-02 22:49:23 -0500

    Testing LCD message Display

--- LCD_Controller.py
+++ LCD_Controller.py
@@ -9,17 +9,16 @@ lcd = I2C_LCD_driver.lcd()
 
 
 def Scroll_Name(display, text='', num_line = 1, num_cols=16):
-
-        if len(text) > num_cols:
-                display.lcd_display_string(text[:num_cols], num_line)
-                sleep(1)
-                for i in range(len(text) - num_cols + 1):
-                        text_to_print = text[i:i+num_cols]
-                        display.lcd_display_string(text_to_print, num_line)
-			sleep(0.2)
-			sleep(1)
-	else:
-		display.lcd_display_string(text, num_line)
+    if len(text) > num_cols:
+        display.lcd_display_string(text[:num_cols], num_line)
+        sleep(1)
+        for i in range(len(text) - num_cols + 1):
+            text_to_print = text[i:i+num_cols]
+            display.lcd_display_string(text_to_print, num_line)
+            sleep(0.2)
+        sleep(1)
+    else:
+        display.lcd_display_string(text, num_line)
 
 def Artist_Info(text = ""): #text to be given getter for artist info
         Scroll_Name(text,1)
