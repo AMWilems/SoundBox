820d7abc403fe9e788580eac50897c2c54b73b8d
Alexander Wilems <alexander.wilems@gmail.com>
2022-04-02 22:23:50 -0500

    Testing LCD message Display

--- LCD_Controller.py
+++ LCD_Controller.py
@@ -7,6 +7,24 @@ import I2C_LCD_driver
 
 lcd = I2C_LCD_driver.lcd()
 
+
+def long_string(display, text='', num_line=1, num_cols=16):
+""" 
+Parameters: (driver, string to print, number of line to print, number of columns of your display)
+Return: This function send to display your scrolling string.
+"""
+	if len(text) > num_cols:
+	    display.lcd_display_string(text[:num_cols], num_line)
+	    sleep(1)
+            for i in range(len(text) - num_cols + 1):
+                text_to_print = text[i:i+num_cols]
+                display.lcd_display_string(text_to_print, num_line)
+		sleep(0.2)
+	    sleep(1)
+	else:
+	    display.lcd_display_string(text, num_line)
+
+
 def Play_Message():
     lcd.lcd_clear()
     lcd.lcd_display_string("Playing...",1,2)
