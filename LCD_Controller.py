dee27fe0fb72902a3adff367afa42f29c5cac047
Alexander Wilems <alexander.wilems@gmail.com>
2022-04-02 22:36:19 -0500

    Testing LCD message Display

--- LCD_Controller.py
+++ LCD_Controller.py
@@ -14,10 +14,10 @@ def Scroll_Name(display, text='', num_line = 1, num_cols=16):
                 display.lcd_display_string(text[:num_cols], num_line)
                 sleep(1)
                 for i in range(len(text) - num_cols + 1):
-			text_to_print = text[i:i+num_cols]
-			display.lcd_display_string(text_to_print, num_line)
+                        text_to_print = text[i:i+num_cols]
+                        display.lcd_display_string(text_to_print, num_line)
 			sleep(0.2)
-		sleep(1)
+			sleep(1)
 	else:
 		display.lcd_display_string(text, num_line)
 
