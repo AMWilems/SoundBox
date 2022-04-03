f2408fabbe1f2e53dd4c9cf36126a145217195c3
Alexander Wilems <alexander.wilems@gmail.com>
2022-04-02 22:07:47 -0500

    Testing LCD message Display

--- LCD_Controller.py
+++ LCD_Controller.py
@@ -10,21 +10,21 @@ lcd = I2C_LCD_driver.lcd()
 def Play_Message():
     lcd.lcd_clear()
     lcd.lcd_display_string("Playing...",1,2)
-
+    Wait_And_Reset()
 def Pause_Message():
     lcd.lcd_clear()
     lcd.lcd_display_string("Pausing...",1,2)
-    Wait_And_Clear()
+    Wait_And_Reset()
 
 def Next_Song_Message():
     lcd.lcd_clear()
     lcd.lcd_display_string("Next Song...",1,0)
-    Wait_And_Clear()
+    Wait_And_Reset()
 
 def Last_Song_Message():
     lcd.lcd_clear()
     lcd.lcd_display_string("Last Song...",1,0)
-    Wait_And_Clear()
+    Wait_And_Reset()
 
 def Read_Card_Message():
     lcd.lcd_clear()
@@ -45,15 +45,15 @@ def Successful():
     lcd.lcd_clear()
     lcd.lcd_display_string("Written",1,0)
     lcd.lcd_display_string("Successfully",2,0)
-    Wait_And_Clear()
+    Wait_And_Reset()
     
 
 def Card_Contents(card_data):
     lcd.lcd_clear()
     lcd.lcd_display_string(card_data,1,0)
-    Wait_And_Clear()
+    Wait_And_Reset()
 
-def Wait_And_Clear():
+def Wait_And_Reset():
     sleep(1.5)
     lcd.lcd_clear()
     
