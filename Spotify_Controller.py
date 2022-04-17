d127fadc21a4dcc245b7b6a4cbf54e8a810fd732
Alexander Wilems <alexander.wilems@gmail.com>
2022-04-17 09:49:09 -0500

    spotify function integration

--- Spotify_Controller.py
+++ Spotify_Controller.py
@@ -3,8 +3,8 @@ from spotipy.oauth2 import SpotifyOAuth
 from time import sleep
 
 SPOTIPY_DEVICE_ID = 'b87867c30c93d315bd7a3a67efd57d7a8888506b' # change to rasperry pi device ID
-SPOTIPY_CLIENT_ID = '0b53ed2e631f47ddb7ef45582f666b71'
-SPOTIPY_CLIENT_SECRET = '47b8d86158f64d8b9cb70360d42bef71'
+SPOTIPY_CLIENT_ID = '2b690c77b1624824bb3209a3dc1f94f4'
+SPOTIPY_CLIENT_SECRET = 'fc9b969b85cf4f2280bde177ca5c243'
 scope = "user-read-playback-state, user-read-currently-playing"
 
 sp = spotipy.Spotify(
